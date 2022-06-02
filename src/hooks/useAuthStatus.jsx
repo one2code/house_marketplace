a1294cdea3032e5971c12a49c91b27425ff3be4f
Travis Bennett <t_bennett_a@protonmail.com>
2022-06-02 14:12:47 -0400

    Changed useEffect and added a useRef for better memory performance, added a spinner for loading status

--- src/hooks/useAuthStatus.jsx
+++ src/hooks/useAuthStatus.jsx
@@ -1,23 +1,27 @@
-import {useEffect, useState} from 'react'
-import {getAuth, onAuthStateChanged} from 'firebase/auth'
+import { useEffect, useState, useRef } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 // Checks to see if the user is logged in
 export const useAuthStatus = () => {
-const [loggedIn, setLoggedIn] = useState(false)
-const [checkingStatus, setCheckingStatus] = useState(true)
+	const [loggedIn, setLoggedIn] = useState(false);
+	const [checkingStatus, setCheckingStatus] = useState(true);
+	const isMounted = useRef(true);
 
-// Passes getAuth() from Firebase as an argument, and checks to see if the user is logged in, if so, then the useEffect will change the checkingStatus state to false and return loggedIn and checkingStatus
-useEffect(() => {
-    const auth = getAuth()
-    onAuthStateChanged(auth, (user) => {
-        if (user) {
-            setLoggedIn(true)
-        }
-        setCheckingStatus(false)
-    })
-})
-
-  return (
-    {loggedIn, checkingStatus}
-  )
-}
+	// Passes getAuth() from Firebase as an argument, and checks to see if the user is logged in, if so, then the useEffect will change the checkingStatus state to false and return loggedIn and checkingStatus
+	useEffect(() => {
+		// isMounted manages any potential memory leaks
+		if (isMounted) {
+			const auth = getAuth();
+			onAuthStateChanged(auth, (user) => {
+				if (user) {
+					setLoggedIn(true);
+				}
+				setCheckingStatus(false);
+			});
+		}
+		return () => {
+			isMounted.current = false;
+		};
+	}, [isMounted]);
+	return { loggedIn, checkingStatus };
+};
